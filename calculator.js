9c4786a44b5f09f0c510fdc4b7b3725924d5e93b
Ender2014 <lau.zihao@gmail.com>
2024-10-08 20:50:43 +0800

    Added separate number and operator classes
    This is so that it is easier to add event listeners.
    This is because operators and operands do different
    things when pressed.

--- calculator.js
+++ calculator.js
@@ -32,12 +32,22 @@ function operate(operator, num1, num2){
     }
 }
 
+let firstOperand = 0;
+let operator = "";
+let secondOperand = 0;
+let displayValue = 0
+
+// display result
 const buttons = document.querySelectorAll("button");
 
 const resultContainer = document.querySelector(".results")
 
 buttons.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        resultContainer.textContent += e.target.textContent;
-    });
+    if(Number.isInteger(+button.textContent)){
+        button.addEventListener("click", (e) => {
+            resultContainer.textContent += e.target.textContent;
+            displayValue = +resultContainer.textContent;
+        }); 
+    }
 });
+
