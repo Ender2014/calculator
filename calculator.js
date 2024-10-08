844b40a4e7a0f1b61802a3b6f6db4dbb1d9313e1
Ender2014 <lau.zihao@gmail.com>
2024-10-08 18:01:48 +0800

    Added basic calculator operation function.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+// Basic calculator functions
 function add(a, b){
     return a + b;
 }
@@ -12,4 +13,21 @@ function multiply(a, b){
 
 function divide(a, b){
     return a / b;
+}
+
+// Calculator Operation
+function operate(operator, num1, num2){
+    switch(operator){
+        case "+":
+            return add(num1, num2);
+
+        case "-":
+            return substract(num1, num2);
+
+        case "*":
+            return multiply(num1, num2);
+
+        case "/":
+            return divide(num1, num2);
+    }
 }
